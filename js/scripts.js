48710a339d680bd2d068fb0dc7a5aed7dad39c0e
Stewart ilondanga <stilondanga@gmail.com>
2019-02-02 10:59:18 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -189,55 +189,55 @@ window.onload = function() {
       }
     }
 
+
+    // Play
+    play = function() {
+      categories = [
+        ["everton", "liverpool", "swansea", "chelsea", "hull", "manchester-city", "newcastle-united"],
+        ["alien", "dirty-harry", "gladiator", "finding-nemo", "jaws"],
+        ["manchester", "milan", "madrid", "amsterdam", "prague"]
+      ];
+
+      chosenCategory = categories[Math.floor(Math.random() * categories.length)];
+      word = chosenCategory[Math.floor(Math.random() * chosenCategory.length)];
+      word = word.replace(/\s/g, "-");
+      console.log(word);
+      buttons();
+
+      geusses = [];
+      lives = 10;
+      counter = 0;
+      space = 0;
+      result();
+      comments();
+      selectCat();
+      canvas();
+    }
+
+    play();
     /*
-                              // Play
-                              play = function () {
-                                categories = [
-                                    ["everton", "liverpool", "swansea", "chelsea", "hull", "manchester-city", "newcastle-united"],
-                                    ["alien", "dirty-harry", "gladiator", "finding-nemo", "jaws"],
-                                    ["manchester", "milan", "madrid", "amsterdam", "prague"]
-                                ];
+                                  // Hint
 
-                                chosenCategory = categories[Math.floor(Math.random() * categories.length)];
-                                word = chosenCategory[Math.floor(Math.random() * chosenCategory.length)];
-                                word = word.replace(/\s/g, "-");
-                                console.log(word);
-                                buttons();
+                                    hint.onclick = function() {
 
-                                geusses = [ ];
-                                lives = 10;
-                                counter = 0;
-                                space = 0;
-                                result();
-                                comments();
-                                selectCat();
-                                canvas();
-                              }
+                                      hints = [
+                                        ["Based in Mersyside", "Based in Mersyside", "First Welsh team to reach the Premier Leauge", "Owned by A russian Billionaire", "Once managed by Phil Brown", "2013 FA Cup runners up", "Gazza's first club"],
+                                        ["Science-Fiction horror film", "1971 American action film", "Historical drama", "Anamated Fish", "Giant great white shark"],
+                                        ["Northern city in the UK", "Home of AC and Inter", "Spanish capital", "Netherlands capital", "Czech Republic capital"]
+                                    ];
 
-                              play();
+                                    var catagoryIndex = categories.indexOf(chosenCategory);
+                                    var hintIndex = chosenCategory.indexOf(word);
+                                    showClue.innerHTML = "Clue: - " +  hints [catagoryIndex][hintIndex];
+                                  };
 
-                              // Hint
+                                   // Reset
 
-                                hint.onclick = function() {
-
-                                  hints = [
-                                    ["Based in Mersyside", "Based in Mersyside", "First Welsh team to reach the Premier Leauge", "Owned by A russian Billionaire", "Once managed by Phil Brown", "2013 FA Cup runners up", "Gazza's first club"],
-                                    ["Science-Fiction horror film", "1971 American action film", "Historical drama", "Anamated Fish", "Giant great white shark"],
-                                    ["Northern city in the UK", "Home of AC and Inter", "Spanish capital", "Netherlands capital", "Czech Republic capital"]
-                                ];
-
-                                var catagoryIndex = categories.indexOf(chosenCategory);
-                                var hintIndex = chosenCategory.indexOf(word);
-                                showClue.innerHTML = "Clue: - " +  hints [catagoryIndex][hintIndex];
-                              };
-
-                               // Reset
-
-                              document.getElementById('reset').onclick = function() {
-                                correct.parentNode.removeChild(correct);
-                                letters.parentNode.removeChild(letters);
-                                showClue.innerHTML = "";
-                                context.clearRect(0, 0, 400, 400);
-                                play();
-                              }
-                            }
+                                  document.getElementById('reset').onclick = function() {
+                                    correct.parentNode.removeChild(correct);
+                                    letters.parentNode.removeChild(letters);
+                                    showClue.innerHTML = "";
+                                    context.clearRect(0, 0, 400, 400);
+                                    play();
+                                  }
+                                }
