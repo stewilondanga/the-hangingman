73ba5ee61e9e1d23a1e1e913067f90a2cb110c46
Stewart ilondanga <stilondanga@gmail.com>
2019-02-02 10:58:04 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -101,143 +101,143 @@ window.onload = function() {
       drawArray[drawMe]();
     }
 
+
+    // Hangman
+    canvas = function() {
+
+      myStickman = document.getElementById("stickman");
+      context = myStickman.getContext('2d');
+      context.beginPath();
+      context.strokeStyle = "#fff";
+      context.lineWidth = 2;
+    };
+
+    head = function() {
+      myStickman = document.getElementById("stickman");
+      context = myStickman.getContext('2d');
+      context.beginPath();
+      context.arc(60, 25, 10, 0, Math.PI * 2, true);
+      context.stroke();
+    }
+
+    draw = function($pathFromx, $pathFromy, $pathTox, $pathToy) {
+
+      context.moveTo($pathFromx, $pathFromy);
+      context.lineTo($pathTox, $pathToy);
+      context.stroke();
+    }
+
+    frame1 = function() {
+      draw(0, 150, 150, 150);
+    };
+
+    frame2 = function() {
+      draw(10, 0, 10, 600);
+    };
+
+    frame3 = function() {
+      draw(0, 5, 70, 5);
+    };
+
+    frame4 = function() {
+      draw(60, 5, 60, 15);
+    };
+
+    torso = function() {
+      draw(60, 36, 60, 70);
+    };
+
+    rightArm = function() {
+      draw(60, 46, 100, 50);
+    };
+
+    leftArm = function() {
+      draw(60, 46, 20, 50);
+    };
+
+    rightLeg = function() {
+      draw(60, 70, 100, 100);
+    };
+
+    leftLeg = function() {
+      draw(60, 70, 20, 100);
+    };
+
+    drawArray = [rightLeg, leftLeg, rightArm, leftArm, torso, head, frame4, frame3, frame2, frame1];
+
     /*
-                       // Hangman
-                      canvas =  function(){
-
-                        myStickman = document.getElementById("stickman");
-                        context = myStickman.getContext('2d');
-                        context.beginPath();
-                        context.strokeStyle = "#fff";
-                        context.lineWidth = 2;
-                      };
-
-                        head = function(){
-                          myStickman = document.getElementById("stickman");
-                          context = myStickman.getContext('2d');
-                          context.beginPath();
-                          context.arc(60, 25, 10, 0, Math.PI*2, true);
-                          context.stroke();
-                        }
-
-                      draw = function($pathFromx, $pathFromy, $pathTox, $pathToy) {
-
-                        context.moveTo($pathFromx, $pathFromy);
-                        context.lineTo($pathTox, $pathToy);
-                        context.stroke();
-                    }
-
-                       frame1 = function() {
-                         draw (0, 150, 150, 150);
-                       };
-
-                       frame2 = function() {
-                         draw (10, 0, 10, 600);
-                       };
-
-                       frame3 = function() {
-                         draw (0, 5, 70, 5);
-                       };
-
-                       frame4 = function() {
-                         draw (60, 5, 60, 15);
-                       };
-
-                       torso = function() {
-                         draw (60, 36, 60, 70);
-                       };
-
-                       rightArm = function() {
-                         draw (60, 46, 100, 50);
-                       };
-
-                       leftArm = function() {
-                         draw (60, 46, 20, 50);
-                       };
-
-                       rightLeg = function() {
-                         draw (60, 70, 100, 100);
-                       };
-
-                       leftLeg = function() {
-                         draw (60, 70, 20, 100);
-                       };
-
-                      drawArray = [rightLeg, leftLeg, rightArm, leftArm,  torso,  head, frame4, frame3, frame2, frame1];
-
-
-                      // OnClick Function
-                       check = function () {
-                        list.onclick = function () {
-                          var geuss = (this.innerHTML);
-                          this.setAttribute("class", "active");
-                          this.onclick = null;
-                          for (var i = 0; i < word.length; i++) {
-                            if (word[i] === geuss) {
-                              geusses[i].innerHTML = geuss;
-                              counter += 1;
+                          // OnClick Function
+                           check = function () {
+                            list.onclick = function () {
+                              var geuss = (this.innerHTML);
+                              this.setAttribute("class", "active");
+                              this.onclick = null;
+                              for (var i = 0; i < word.length; i++) {
+                                if (word[i] === geuss) {
+                                  geusses[i].innerHTML = geuss;
+                                  counter += 1;
+                                }
+                              }
+                              var j = (word.indexOf(geuss));
+                              if (j === -1) {
+                                lives -= 1;
+                                comments();
+                                animate();
+                              } else {
+                                comments();
+                              }
                             }
                           }
-                          var j = (word.indexOf(geuss));
-                          if (j === -1) {
-                            lives -= 1;
+
+
+                          // Play
+                          play = function () {
+                            categories = [
+                                ["everton", "liverpool", "swansea", "chelsea", "hull", "manchester-city", "newcastle-united"],
+                                ["alien", "dirty-harry", "gladiator", "finding-nemo", "jaws"],
+                                ["manchester", "milan", "madrid", "amsterdam", "prague"]
+                            ];
+
+                            chosenCategory = categories[Math.floor(Math.random() * categories.length)];
+                            word = chosenCategory[Math.floor(Math.random() * chosenCategory.length)];
+                            word = word.replace(/\s/g, "-");
+                            console.log(word);
+                            buttons();
+
+                            geusses = [ ];
+                            lives = 10;
+                            counter = 0;
+                            space = 0;
+                            result();
                             comments();
-                            animate();
-                          } else {
-                            comments();
+                            selectCat();
+                            canvas();
+                          }
+
+                          play();
+
+                          // Hint
+
+                            hint.onclick = function() {
+
+                              hints = [
+                                ["Based in Mersyside", "Based in Mersyside", "First Welsh team to reach the Premier Leauge", "Owned by A russian Billionaire", "Once managed by Phil Brown", "2013 FA Cup runners up", "Gazza's first club"],
+                                ["Science-Fiction horror film", "1971 American action film", "Historical drama", "Anamated Fish", "Giant great white shark"],
+                                ["Northern city in the UK", "Home of AC and Inter", "Spanish capital", "Netherlands capital", "Czech Republic capital"]
+                            ];
+
+                            var catagoryIndex = categories.indexOf(chosenCategory);
+                            var hintIndex = chosenCategory.indexOf(word);
+                            showClue.innerHTML = "Clue: - " +  hints [catagoryIndex][hintIndex];
+                          };
+
+                           // Reset
+
+                          document.getElementById('reset').onclick = function() {
+                            correct.parentNode.removeChild(correct);
+                            letters.parentNode.removeChild(letters);
+                            showClue.innerHTML = "";
+                            context.clearRect(0, 0, 400, 400);
+                            play();
                           }
                         }
-                      }
-
-
-                      // Play
-                      play = function () {
-                        categories = [
-                            ["everton", "liverpool", "swansea", "chelsea", "hull", "manchester-city", "newcastle-united"],
-                            ["alien", "dirty-harry", "gladiator", "finding-nemo", "jaws"],
-                            ["manchester", "milan", "madrid", "amsterdam", "prague"]
-                        ];
-
-                        chosenCategory = categories[Math.floor(Math.random() * categories.length)];
-                        word = chosenCategory[Math.floor(Math.random() * chosenCategory.length)];
-                        word = word.replace(/\s/g, "-");
-                        console.log(word);
-                        buttons();
-
-                        geusses = [ ];
-                        lives = 10;
-                        counter = 0;
-                        space = 0;
-                        result();
-                        comments();
-                        selectCat();
-                        canvas();
-                      }
-
-                      play();
-
-                      // Hint
-
-                        hint.onclick = function() {
-
-                          hints = [
-                            ["Based in Mersyside", "Based in Mersyside", "First Welsh team to reach the Premier Leauge", "Owned by A russian Billionaire", "Once managed by Phil Brown", "2013 FA Cup runners up", "Gazza's first club"],
-                            ["Science-Fiction horror film", "1971 American action film", "Historical drama", "Anamated Fish", "Giant great white shark"],
-                            ["Northern city in the UK", "Home of AC and Inter", "Spanish capital", "Netherlands capital", "Czech Republic capital"]
-                        ];
-
-                        var catagoryIndex = categories.indexOf(chosenCategory);
-                        var hintIndex = chosenCategory.indexOf(word);
-                        showClue.innerHTML = "Clue: - " +  hints [catagoryIndex][hintIndex];
-                      };
-
-                       // Reset
-
-                      document.getElementById('reset').onclick = function() {
-                        correct.parentNode.removeChild(correct);
-                        letters.parentNode.removeChild(letters);
-                        showClue.innerHTML = "";
-                        context.clearRect(0, 0, 400, 400);
-                        play();
-                      }
-                    }
